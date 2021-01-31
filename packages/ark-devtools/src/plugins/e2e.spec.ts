f2443fd8f13a182f4946f460ebec3faab71c0c40
Dameem Shahabaz <dz@skyslit.com>
2021-02-01 00:27:38 +0530

    fpz command issue fixed (#10)
    * fix: issue with typescript version
    
    * fix(devtools): moved execa from dev dep to prod dep
    
    * test: added test user in local git
    
    * test(devtools): disabled git commit in test mode
    
    * test(devtools): disabled git commit in test mode
    
    * test(devtools): disabled git commit in test mode
    
    * test(devtools): disabled git commit in test mode
    
    * test(devtools): disabled git commit in test mode

--- packages/ark-devtools/src/plugins/e2e.spec.ts
+++ packages/ark-devtools/src/plugins/e2e.spec.ts
@@ -13,6 +13,8 @@ const testDir = path.join(
   '../../../../../__ark_automated_test_artifacts__/plugin-e2e'
 );
 
+process.env.git_testmode = 'true';
+
 beforeEach(() => {
   rimraf.sync(testDir);
   fs.mkdirSync(testDir, { recursive: true });
