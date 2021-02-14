729dccbaa0d44789af941f51092aeebeb7471e34
Dameem Shahabaz <dz@skyslit.com>
2021-02-14 12:27:43 +0530

    refactor: removed unnecessary automation code in cli

--- packages/ark-cli/src/index-backup.tsx
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import React from 'react';
-import { render } from 'ink';
-import App from './app';
-import { getAppProps } from './cli';
-
-render(<App {...getAppProps()} />);
