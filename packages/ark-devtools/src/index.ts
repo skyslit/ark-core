955fd920bf5cba8b99754a230a7ae09ac9722f92
Dameem Shahabaz <dz@skyslit.com>
2021-02-14 14:06:43 +0530

    refactor: removed automation and manifest implementations

--- packages/ark-devtools/src/index.ts
+++ packages/ark-devtools/src/index.ts
@@ -1,29 +1,2 @@
-import writePackageManifest from './automation/processes/manifest/writePackageManifest';
-import LongTestProcess from './automation/processes/LongTestProcess/TestProcess';
-import SyncProcess from './automation/processes/Sync/SyncProject';
-import { createProcess } from './automation/core/Automator';
-export { Job, Automator } from './automation/core/Automator';
-export {
-  ManifestManager,
-  InvalidManifestError,
-  ManifestUtils,
-} from './utils/ManifestManager';
-export {
-  JobSnapshot,
-  AutomationSnapshot,
-  StepSnapshot,
-  WorkerStatus,
-} from './automation/core/Automator';
 export { BackendBuilder } from './builders/BackendBuilder';
 export { SPABuilder } from './builders/FrontendBuilder';
-
-export const Automations = {
-  processes: {
-    writePackageManifest,
-    LongTestProcess,
-    SyncProcess,
-  },
-  utils: {
-    createProcess,
-  },
-};
