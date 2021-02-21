ea1c339a9ddb440db6057358fdc57036942a833e
Dameem Shahabaz <dz@skyslit.com>
2021-02-22 01:18:18 +0530

    refactor: removed unnecessary dependencies

--- packages/ark-devtools/src/index.ts
+++ packages/ark-devtools/src/index.ts
@@ -1,10 +1,2 @@
 export { BackendBuilder } from './builders/BackendBuilder';
 export { SPABuilder } from './builders/FrontendBuilder';
-
-import initializeNpm from './activities/init/npm-init';
-
-export const Activities = {
-  init: {
-    initializeNpm,
-  },
-};
